1c52eed08e715900884bc6c4e07ea5eccc96e943
schummelbiene <schummelbiene@web.de>
2015-03-13 23:50:16 +0100

    A first partially working draft. There's still a lot to do.

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct vertex vertex_t;
 
 typedef struct {
 	vertex_t* head;
-	int lenght;
+	int length;
 	
 }list_t;
 
@@ -19,7 +19,7 @@ typedef struct {
 // Creates a BRILLIANT (not really new, because we already have the pointer to it), empty list
 
 void list_init(list_t* l) {
-	l->next = NULL;
+	l->head = NULL;
 	l->length = 0;
 };
 
@@ -30,10 +30,22 @@ void list_init(list_t* l) {
 void list_insert(list_t* l, int val_new, int pos) {
 
 	//	l->head =def= (*l).head;
-	pos = pos % l->length;
 	vertex_t* ver_new = malloc(sizeof(vertex_t));
-	//	vertex_t ver_new = *malloc(sizeof(vertex_t)); falsch, lokal
 	ver_new->v = val_new;
+	
+	if(l->length == 0) {
+		l->head = ver_new;
+		ver_new->next = ver_new;
+		//		i += 1 =def= i++ =def= i = i + 1
+		//    x *= 2 =def= x <<= 1
+		l->length = 1;
+		return;
+	}
+	
+	pos = pos % l->length;
+
+	//	vertex_t ver_new = *malloc(sizeof(vertex_t)); falsch, lokal
+
 
 	vertex_t* ver_iter = l->head;
 	
@@ -62,11 +74,9 @@ void list_delete(list_t* l, int pos) {
 
 	vertex_t* ver_iter = l->head;
 	
-	
-
 	for(int i = 0; i < pos-1; i++) {
 		ver_iter = ver_iter->next;
-	};
+	}
 	
 
 	vertex_t* ver_help = ver_iter->next;
@@ -75,14 +85,27 @@ void list_delete(list_t* l, int pos) {
 	free(ver_help);
 
 	l->length--;
-};
+}
+
+void list_print(list_t* l) {
+	vertex_t* ver_help = l->head;
+	
+	for(int i = 0; i< l->length; i++) {
+		printf("%i ", ver_help->v);
+		ver_help = ver_help->next;
+	}
+	printf("\n");
+}
 
 void main(int argc, char** argv) {
-	int a[2] = {1, 2};
-	int* b = &a[0];
-	int c = *b;
 
+	list_t mylist;
 	
+  list_init (&mylist); // Adresse
+	list_insert (&mylist, 6, 0);
+	list_insert (&mylist, 8, 1);
+	list_print(&mylist);
+	list_delete(&mylist, 0);
 }
 
 
